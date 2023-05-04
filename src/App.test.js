8ec55dd02b076e88f7415861e164bb30b7295773
godsfavours <67076777+godsfavours@users.noreply.github.com>
2023-05-04 14:16:44 -0700

    add tests for App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders jammming header text', () => {
+beforeEach(() => {
   render(<App />);
-  const linkElement = screen.getByText(/jammming/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+describe('app', () => {
+  test('renders header section', () => {
+    const headerText = screen.getByText(/jammming/i);
+    expect(headerText).toBeInTheDocument();
+  });
+
+  test('renders search section', () => {
+    const searchHeading = screen.getByText(/search results/i);
+    expect(searchHeading).toBeInTheDocument();
+  });
+
+  test('renders playlist section', () => {
+    const playlistHeading = screen.getByText((content, element) => {
+      return element.tagName.toLowerCase() === 'h2' && content === 'Create a Playlist';
+    })
+    expect(playlistHeading).toBeInTheDocument();
+  });
 });
